65ef52cda14c46a1badd6824057012c40a7f871d
Nikolai4ac <Nikolai4ac@students.softuni.bg>
2024-10-31 04:07:21 +0200

    Remove unused variable

--- public/js/notFoundRedirect.js
+++ public/js/notFoundRedirect.js
@@ -1,15 +1,13 @@
 let htmlText = document.getElementById("redirectMsg");
-
-let timer = 5000;
 let delay = 5;
 let newText = "";
 let delayIndexInMessage = htmlText.innerHTML.indexOf(delay); //in this case we have 5 seconds delay
 let timeReached = false;
 setInterval(() => {
-  delay--; //goes 4 in first i
-  newText = htmlText.innerHTML.replace(htmlText.innerHTML[delayIndexInMessage],delay ); /*newText is replacing the item on the position of first delay number (in this case 5) with the counter number, which on first iteration becomes 4.*/ 
-  htmlText.innerHTML = newText; //we're replacing the default message with the new updated one.
-  if (delay == 0) {
-    window.location.href = "/";
-  }
+   delay--; //goes 4 in first i
+   newText = htmlText.innerHTML.replace(htmlText.innerHTML[delayIndexInMessage],delay ); /*newText is replacing the item on the position of first delay number (in this case 5) with the counter number, which on first iteration becomes 4.*/ 
+   htmlText.innerHTML = newText; //we're replacing the default message with the new updated one.
+   if (delay == 0) {
+     window.location.href = "/";
+   }
 }, 1000);
