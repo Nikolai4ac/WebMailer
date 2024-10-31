f51bb5448bc15e1f8402ef39f39fe50786acffa2
Nikolai4ac <Nikolai4ac@students.softuni.bg>
2024-10-31 04:05:33 +0200

    Simplify code logic and improve readability

--- public/js/notFoundRedirect.js
+++ public/js/notFoundRedirect.js
@@ -7,11 +7,7 @@ let delayIndexInMessage = htmlText.innerHTML.indexOf(delay); //in this case we h
 let timeReached = false;
 setInterval(() => {
   delay--; //goes 4 in first i
-  newText = htmlText.innerHTML.replace(
-    htmlText.innerHTML[delayIndexInMessage],
-    delay
-  ); /*newText is replacing the item on the position 
-            of first delay number (in this case 5) with the counter number, which on first iteration becomes 4.*/
+  newText = htmlText.innerHTML.replace(htmlText.innerHTML[delayIndexInMessage],delay ); /*newText is replacing the item on the position of first delay number (in this case 5) with the counter number, which on first iteration becomes 4.*/ 
   htmlText.innerHTML = newText; //we're replacing the default message with the new updated one.
   if (delay == 0) {
     window.location.href = "/";
